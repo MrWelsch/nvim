020d905860b6f51edb20760c79a1b52730a3d2ed
MrKumata <n.Welsch@protonmail.com>
2021-06-12 15:14:25 +0200

    chore(folder structure) Refactoring

---
+++ lua/plugins/config/galaxyline.lua
@@ -0,0 +1,219 @@
+-- TODO: Check out lualine
+local gl = require('galaxyline')
+local colors = {
+    -- Dracula_Pro
+    -- bg = '#22212C',
+    -- Dracula_Pro_Helsing
+    bg = '#0B0D0F',
+    yellow = '#FFFF80',
+    dark_yellow = '#FFFF80',
+    cyan = '#80FFEA',
+    green = '#8AFF80',
+    light_green = '#8AFF80',
+    string_orange = '#FFCA80',
+    orange = '#FFCA80',
+    purple = '#9580FF',
+    magenta = '#FF80BF',
+    grey = '7970A9',
+    blue = '#80FFEA',
+    vivid_blue = '#80FFEA',
+    light_blue = '#80FFEA',
+    red = '#FF9580',
+    error_red = '#FF9580',
+    info_yellow = '#FFFF80'
+}
+local condition = require('galaxyline.condition')
+local gls = gl.section
+gl.short_line_list = {'NvimTree', 'vista', 'dbui', 'packer'}
+
+table.insert(gls.left, {
+    ViMode = {
+        provider = function()
+            -- auto change color according the vim mode
+            local mode_color = {
+                n = colors.blue,
+                i = colors.green,
+                v = colors.purple,
+                [''] = colors.purple,
+                V = colors.purple,
+                c = colors.magenta,
+                no = colors.blue,
+                s = colors.orange,
+                S = colors.orange,
+                [''] = colors.orange,
+                ic = colors.yellow,
+                R = colors.red,
+                Rv = colors.red,
+                cv = colors.blue,
+                ce = colors.blue,
+                r = colors.cyan,
+                rm = colors.cyan,
+                ['r?'] = colors.cyan,
+                ['!'] = colors.blue,
+                t = colors.blue
+            }
+            vim.api.nvim_command('hi GalaxyViMode guifg=' .. mode_color[vim.fn.mode()])
+            return '▊ '
+        end,
+        highlight = {colors.red, colors.bg}
+    }
+})
+print(vim.fn.getbufvar(0, 'ts'))
+vim.fn.getbufvar(0, 'ts')
+
+table.insert(gls.left, {
+    GitIcon = {
+        provider = function()
+            return ' '
+        end,
+        condition = condition.check_git_workspace,
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.orange, colors.bg}
+    }
+})
+
+table.insert(gls.left, {
+    GitBranch = {
+        provider = 'GitBranch',
+        condition = condition.check_git_workspace,
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.grey, colors.bg}
+    }
+})
+
+table.insert(gls.left, {
+    DiffAdd = {
+        provider = 'DiffAdd',
+        condition = condition.hide_in_width,
+        icon = '  ',
+        highlight = {colors.green, colors.bg}
+    }
+})
+table.insert(gls.left, {
+    DiffModified = {
+        provider = 'DiffModified',
+        condition = condition.hide_in_width,
+        icon = ' 柳',
+        highlight = {colors.blue, colors.bg}
+    }
+})
+table.insert(gls.left, {
+    DiffRemove = {
+        provider = 'DiffRemove',
+        condition = condition.hide_in_width,
+        icon = '  ',
+        highlight = {colors.red, colors.bg}
+    }
+})
+
+table.insert(gls.right, {
+    DiagnosticError = {provider = 'DiagnosticError', icon = '  ', highlight = {colors.error_red, colors.bg}}
+})
+table.insert(gls.right, {DiagnosticWarn = {provider = 'DiagnosticWarn', icon = '  ', highlight = {colors.orange, colors.bg}}})
+
+table.insert(gls.right, {
+    DiagnosticHint = {provider = 'DiagnosticHint', icon = '  ', highlight = {colors.vivid_blue, colors.bg}}
+})
+
+table.insert(gls.right, {DiagnosticInfo = {provider = 'DiagnosticInfo', icon = '  ', highlight = {colors.info_yellow, colors.bg}}})
+
+table.insert(gls.right, {
+    TreesitterIcon = {
+        provider = function()
+            if next(vim.treesitter.highlighter.active) ~= nil then return ' ' end
+            return ''
+        end,
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.green, colors.bg}
+    }
+})
+
+table.insert(gls.right, {
+    ShowLspClient = {
+        provider = 'GetLspClient',
+        condition = function()
+            local tbl = {['dashboard'] = true, [' '] = true}
+            if tbl[vim.bo.filetype] then return false end
+            return true
+        end,
+        icon = '  ',
+        highlight = {colors.grey, colors.bg}
+    }
+})
+
+table.insert(gls.right, {
+    LineInfo = {
+        provider = 'LineColumn',
+        separator = '  ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.grey, colors.bg}
+    }
+})
+
+table.insert(gls.right, {
+    PerCent = {
+        provider = 'LinePercent',
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.grey, colors.bg}
+    }
+})
+
+table.insert(gls.right, {
+    Tabstop = {
+        provider = function()
+            return "Spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth") .. " "
+        end,
+        condition = condition.hide_in_width,
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.grey, colors.bg}
+    }
+})
+
+table.insert(gls.right, {
+    BufferType = {
+        provider = 'FileTypeName',
+        condition = condition.hide_in_width,
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.grey, colors.bg}
+    }
+})
+
+table.insert(gls.right, {
+    FileEncode = {
+        provider = 'FileEncode',
+        condition = condition.hide_in_width,
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.grey, colors.bg}
+    }
+})
+
+table.insert(gls.right, {
+    Space = {
+        provider = function()
+            return ' '
+        end,
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.orange, colors.bg}
+    }
+})
+
+table.insert(gls.short_line_left, {
+    BufferType = {
+        provider = 'FileTypeName',
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.grey, colors.bg}
+    }
+})
+
+table.insert(gls.short_line_left, {
+    SFileName = {provider = 'SFileName', condition = condition.buffer_not_empty, highlight = {colors.grey, colors.bg}}
+})
