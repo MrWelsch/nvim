6676bb9f0743feccc92256a466545b8865d8a6b8
MrKumata <n.Welsch@protonmail.com>
2021-06-15 15:51:37 +0200

    chore(compe.lua) Indentation

--- lua/plugins/config/compe.lua
+++ lua/plugins/config/compe.lua
@@ -41,26 +41,26 @@ local check_back_space = function()
 end
 
 -- Use (s-)tab to:
-  --- move to prev/next item in completion menuone
-  --- jump to prev/next snippet's placeholder
-  _G.tab_complete = function()
-	if vim.fn.pumvisible() == 1 then
-	  return t "<C-n>"
-	elseif vim.fn.call("vsnip#available", {1}) == 1 then
-	  return t "<Plug>(vsnip-expand-or-jump)"
-	elseif check_back_space() then
-	  return t "<Tab>"
-	else
-	  return vim.fn['compe#complete']()
-	end
-  end
-  _G.s_tab_complete = function()
-	if vim.fn.pumvisible() == 1 then
-	  return t "<C-p>"
-	elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-	  return t "<Plug>(vsnip-jump-prev)"
-	else
-	  -- If <S-Tab> is not working in your terminal, change it to <C-h>
-	  return t "<S-Tab>"
-	end
-  end
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+_G.tab_complete = function()
+if vim.fn.pumvisible() == 1 then
+	return t "<C-n>"
+elseif vim.fn.call("vsnip#available", {1}) == 1 then
+	return t "<Plug>(vsnip-expand-or-jump)"
+elseif check_back_space() then
+	return t "<Tab>"
+else
+	return vim.fn['compe#complete']()
+end
+end
+_G.s_tab_complete = function()
+if vim.fn.pumvisible() == 1 then
+	return t "<C-p>"
+elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+	return t "<Plug>(vsnip-jump-prev)"
+else
+	-- If <S-Tab> is not working in your terminal, change it to <C-h>
+	return t "<S-Tab>"
+end
+end
