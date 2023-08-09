f292b1a40c6a757fdc7a78e6791d141b626c9528
MrWelsch <n.Welsch@protonmail.com>
2023-08-09 10:41:30 +0200

    Changed Gitsigns Config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -19,8 +19,8 @@ gitsigns.setup {
         interval = 1000,
         follow_files = true,
     },
-    attach_to_untracked = true,
-    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    attach_to_untracked = false,
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
