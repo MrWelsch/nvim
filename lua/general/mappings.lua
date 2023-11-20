e0b6bd82376f332f0c377f9f3fa41a3334ee031b
MrWelsch <n.Welsch@protonmail.com>
2023-11-20 15:20:54 +0100

    Replaced Nvim-Tree With Oil

--- lua/general/mappings.lua
+++ lua/general/mappings.lua
@@ -1,5 +1,15 @@
 local opts = {noremap=true, silent=true}
-local function map(mode, key, mapping) vim.api.nvim_set_keymap(mode, key, mapping, opts) end
+
+local function map(mode, key, mapping) 
+    vim.api.nvim_set_keymap(mode, key, mapping, opts) 
+end
+
+-- local cdh_map = vim.keymap.set
+
+local function swap_map(lhs, rhs, mode)
+    map(mode or "", lhs, rhs, {})
+    map(mode or "", rhs, lhs, {})
+end
 
 --> LEADER
 map('n', '<Space>', '<NOP>')
@@ -55,10 +65,49 @@ map('n', '<Leader>x',   ':!chmod +x %<CR>', { silent = true })
 map('n', '<Leader>z',   ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
 
 --> EXPLORER
-map('n', '<Leader>e',   ':NvimTreeToggle<CR>')
+-- map('n', '<Leader>e',   ':NvimTreeToggle<CR>')
+-- Get to paren dir by using '-'.
+map('n', '<Leader>e', ':Oil --float<CR>')
 
 --> TERMINAL
 map('n', '<Leader>t',   ':ToggleTerm<CR>')
 
 --> DASHBOARD
 map('n', '<Leader>;',   ':lua MiniStarter.open()<CR>')
+ 
+--------------------------------------------------
+--> Colemak
+--------------------------------------------------
+-- This swaps the maps defined in the colemak_maps table with each other.
+local colemak_maps = {
+{ "n", "j" }, -- down
+{ "e", "k" }, -- up
+{ "s", "h" }, -- left
+{ "t", "l" }, -- right
+}
+
+-- Takes these key mappings and then swaps them for lowercase, uppercase, and <C-w><C- mappings, so you can navigate splits.
+local mvmnt_prefix = "<C-w><C-"
+
+for _, pairs in ipairs(colemak_maps) do
+    local lhs = pairs[1]
+    local rhs = pairs[2]
+
+    local mvmnt_lhs = table.concat({ mvmnt_prefix, lhs, ">" })
+    local mvmnt_rhs = table.concat({ mvmnt_prefix, rhs, ">" })
+
+    -- lowercase
+    swap_map(lhs, rhs)
+
+    -- uppercase
+    swap_map(string.upper(lhs), string.upper(rhs))
+
+    -- window movement
+    swap_map(mvmnt_lhs, mvmnt_rhs)
+end
+
+-- Sets up navigating visual lines as opposed to the real lines, for example in the case of wrapped text.
+-- for _, mode in pairs({ "n", "v" }) do
+--     map(mode, "e", "v:count == 0 ? 'gk' : 'k'", expr)
+--     map(mode, "n", "v:count == 0 ? 'gj' : 'j'", expr)
+-- end 
