9dbdb2a5682e70a45abe7386e8ea20a3c55187d7
MrWelsch <n.Welsch@protonmail.com>
2024-01-24 07:47:54 +0100

    Changed Git Keybinds

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,153 +1,233 @@
-return { 
-    'folke/which-key.nvim', 
-    event = 'VeryLazy',
-    config = function()
-        local status_ok, key = pcall(require, "which-key")
-        if not status_ok then
-            return
-        end
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	config = function()
+		local status_ok, key = pcall(require, "which-key")
+		if not status_ok then
+			return
+		end
 
-        key.setup {
-            plugins = {
-                marks = true, -- shows a list of your marks on ' and `
-                registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-                -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-                -- No actual key bindings are created
-                presets = {
-                    operators = false, -- adds help for operators like d, y, ...
-                    motions = false, -- adds help for motions
-                    text_objects = false, -- help for text objects triggered after entering an operator
-                    windows = true, -- default bindings on <c-w>
-                    nav = true, -- misc bindings to work with windows
-                    z = true, -- bindings for folds, spelling and others prefixed with z
-                    g = true -- bindings for prefixed with g
-                }
-            },
-            icons = {
-                breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-                separator = "➜", -- symbol used between a key and it's label
-                group = "" -- symbol prepended to a group
-            },
-            window = {
-                border = require"general.misc".border, -- none, single, double, shadow TODO: Use this, to refactor other stuff into general files
-                position = "bottom", -- bottom, top
-                margin = {1, 0, 1, 0}, -- extra window margin [top, right, bottom, left]
-                padding = {2, 2, 2, 2}, -- extra window padding [top, right, bottom, left]
-                winblend = 0 -- value between 0-100 0 for fully opaque and 100 for fully transparent
-            },
-            layout = {
-                height = {min = 4, max = 25}, -- min and max height of the columns
-                width = {min = 20, max = 50}, -- min and max width of the columns
-                spacing = 3 -- spacing between columns
-            },
-            hidden = {"<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ "}, -- hide mapping boilerplate
-            show_help = false -- show help message on the command line when the popup is visible
-        }
+		key.setup({
+			plugins = {
+				marks = true, -- shows a list of your marks on ' and `
+				registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+				-- the presets plugin, adds help for a bunch of default keybindings in Neovim
+				-- No actual key bindings are created
+				presets = {
+					operators = false, -- adds help for operators like d, y, ...
+					motions = false, -- adds help for motions
+					text_objects = false, -- help for text objects triggered after entering an operator
+					windows = true, -- default bindings on <c-w>
+					nav = true, -- misc bindings to work with windows
+					z = true, -- bindings for folds, spelling and others prefixed with z
+					g = true, -- bindings for prefixed with g
+				},
+			},
+			icons = {
+				breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+				separator = "➜", -- symbol used between a key and it's label
+				group = "", -- symbol prepended to a group
+			},
+			window = {
+				border = require("general.misc").border, -- none, single, double, shadow TODO: Use this, to refactor other stuff into general files
+				position = "bottom", -- bottom, top
+				margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+				padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+				winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+			},
+			layout = {
+				height = { min = 4, max = 25 }, -- min and max height of the columns
+				width = { min = 20, max = 50 }, -- min and max width of the columns
+				spacing = 3, -- spacing between columns
+			},
+			hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+			show_help = false, -- show help message on the command line when the popup is visible
+		})
 
-        --> OPTIONS APPLIED TO EACH MAPPING
-        -- LEADER
-        local leader_opts = {
-            mode = "n", -- NORMAL mode
-            prefix = "<leader>",
-            buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-            silent = true, -- use `silent` when creating keymaps
-            noremap = true, -- use `noremap` when creating keymaps
-            nowait = false -- use `nowait` when creating keymaps
-        }
-        -- G
-        local g_opts = {
-            mode = "n", -- NORMAL mode
-            prefix = "g",
-            buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-            silent = true, -- use `silent` when creating keymaps
-            noremap = true, -- use `noremap` when creating keymaps
-            nowait = false -- use `nowait` when creating keymaps
-        }
+		--> OPTIONS APPLIED TO EACH MAPPING
+		-- LEADER
+		local leader_opts = {
+			mode = "n", -- NORMAL mode
+			prefix = "<leader>",
+			buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+			silent = true, -- use `silent` when creating keymaps
+			noremap = true, -- use `noremap` when creating keymaps
+			nowait = false, -- use `nowait` when creating keymaps
+		}
+		-- G
+		local g_opts = {
+			mode = "n", -- NORMAL mode
+			prefix = "g",
+			buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+			silent = true, -- use `silent` when creating keymaps
+			noremap = true, -- use `noremap` when creating keymaps
+			nowait = false, -- use `nowait` when creating keymaps
+		}
 
-        --> SET MAPPINGS (Icons: https://www.nerdfonts.com/cheat-sheet)
-        -- LEADER
-        local leader_mappings = {
-            -- STANDARD MAPPINGS
-            [";"] =                                                                                                                 " DASHBOARD",
-            ["/"] =                                                                                                                 " COMMENT",
-            ["e"] =                                                                                                                 " EXPLORER",
-            ["h"] =                                                                                                                 "󰞒 HORIZONTAL SPLIT",
-            ["q"] =                                                                                                                 " QUIT",
-            ["p"] =                                                                                                                 " PERFORMANCE",
-            ["s"] =                                                                                                                 " SAVE",
-            ["z"] =                                                                                                                 " REPLACE",
-            ["x"] =                                                                                                                 " MAKE EXECUTABLE",
-            ["t"] =                                                                                                                 " TERMINAL",
-            ["v"] =                                                                                                                 "󰞔 VERTICAL SPLIT",
-            ["r"] =                                                                                                                 " RENAME BUFFER",
-            ["w"] =                                                                                                                 " CLOSE BUFFER",
-            -- MENUS
-            l = {
-                name = " LSP",
-                d = {"<cmd>lua require('telescope.builtin').diagnostics(require('plugins.telescope').document)<cr>",                "󱪚 DOCUMENT DIAGNOSTICS"},
-                -- D = {"<cmd>lua require('trouble').toggle('document_diagnostics')<cr>",                         "󱪚 TROUBLE DOCUMENT"},
-                w = {"<cmd>lua require('telescope.builtin').diagnostics(require('plugins.telescope'))<cr>",                         " WORKSPACE DIAGNOSTICS"},
-                -- W = {"<cmd>lua require('trouble').toggle('workspace_diagnostics')<cr>",                         " TROUBLE WORKSPACE"},
-                f = {"<cmd>lua vim.lsp.buf.format({ async = false })<cr>",                                                          "󱐀 FORMAT"},
-                i = {"<cmd>LspInfo<cr>",                                                                                            " INFO"},
-                m = {
-                    name = " MARKDOWN",
-                    p = {"<cmd>Glow<cr>",                                                                                           " MARKDOWN PREVIEW"},
-                },
-                t = {
-                    name = "󱁢 TERRAFORM",
-                    i = {"<cmd>:!terraform init<cr>",                                                                               " INIT"},
-                    v = {"<cmd>:!terraform validate<cr>",                                                                           " VALIDATE"},
-                    p = {"<cmd>:!terraform plan<cr>",                                                                               " PLAN"},
-                    a = {"<cmd>:!terraform apply -auto-approve<cr>",                                                                " APPLY"},
-                },
-            },
-            f = {
-                name = " FIND",
-                f = {"<cmd>lua require('telescope.builtin').find_files(require('plugins.telescope'))<cr>",                          "󰈞 FILE"},
-                m = {"<cmd>lua require('telescope.builtin').marks(require('plugins.telescope'))<cr>",                               " MARKS"},
-                M = {"<cmd>lua require('telescope.builtin').man_pages(require('plugins.telescope'))<cr>",                           " MAN PAGES"},
-                p = {"<cmd>lua require'telescope'.extensions.projects.projects{}<cr>",                                              "󱈆 PROJECTS"},
-                r = {"<cmd>lua require('telescope.builtin').oldfiles(require('plugins.telescope'))<cr>",                            "󰡦 RECENT FILES"},
-                R = {"<cmd>lua require('telescope.builtin').registers(require('plugins.telescope'))<cr>",                           "󱝩 REGISTERS"},
-                t = {"<cmd>lua require('telescope.builtin').live_grep(require('plugins.telescope'))<cr>",                           "󱘢 TEXT"},
-                d = {"<cmd>:TodoTelescope<cr>",                                                                                     " TODO"},
-                H = {"<cmd>:Telescope harpoon marks<cr>",                                                                           " HARPOON MARKS"},
-                a = {"<cmd>lua require('harpoon.mark').add_file()<cr>",                                                             "󱝁 HARPOON ADD"},
-                h = {"<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",                                                      " HARPOON MENU"},
-            },
-            c = {
-                name = " CONFIG",
-                p = {"<cmd>Lazy profile<cr>",                                                                                       " PROFILE"},
-                s = {"<cmd>Lazy sync<cr>",                                                                                          " SYNC"},
-                m = {"<cmd>Mason<cr>",                                                                                              "󰰑 MASON"},    
-                t = {"<cmd>lua require('telescope.builtin').colorscheme(require('plugins.telescope'))<cr>",                         " THEME"},
-            },
-            g = {
-                name = "󰊢 GIT",
-                -- BRANCHES ONLY WORK INSIDE GIT REP (Maybe catch exception?) 
-                f = {"<cmd>:GitBlameToggle<cr>",                                                                                    " BLAME"},
-                b = {"<cmd>lua require('telescope.builtin').git_branches(require('plugins.telescope'))<cr>",                        " BRANCHES"},
-                l = {"<cmd>lua _lazygit_toggle()<cr>",                                                                              " LAZYGIT"},    
-            },
-        }
-        -- G
-        local g_mappings = {
-            -- STANDARD MAPPINGS
-            ["a"] =                                                                                                                 "Code Action",
-            ["d"] =                                                                                                                 "Go to definition",
-            ["t"] =                                                                                                                 "Go to type definition",
-            ["D"] =                                                                                                                 "Go to declaration",
-            ["r"] =                                                                                                                 "Show references",
-            ["l"] =                                                                                                                 "Start line diagnostics",
-            ["i"] =                                                                                                                 "Go to Implementation",
-            ["h"] =                                                                                                                 "Hover",
-            ["f"] =                                                                                                                 "Go to References | Implementation",
-        }
-        --> APPLY MAPPINGS AND OPTIONS
-        local wk = require("which-key")
-        wk.register(leader_mappings, leader_opts)
-        wk.register(g_mappings, g_opts)
-    end
+		--> SET MAPPINGS (Icons: https://www.nerdfonts.com/cheat-sheet)
+		-- LEADER
+		local leader_mappings = {
+			-- STANDARD MAPPINGS
+			[";"] = " DASHBOARD",
+			["/"] = " COMMENT",
+			["e"] = " EXPLORER",
+			["h"] = "󰞒 HORIZONTAL SPLIT",
+			["q"] = " QUIT",
+			["p"] = " PERFORMANCE",
+			["s"] = " SAVE",
+			["z"] = " REPLACE",
+			["x"] = " MAKE EXECUTABLE",
+			["t"] = " TERMINAL",
+			["v"] = "󰞔 VERTICAL SPLIT",
+			["r"] = " RENAME BUFFER",
+			["w"] = " CLOSE BUFFER",
+			-- MENUS
+			l = {
+				name = " LSP",
+				d = {
+					"<cmd>lua require('telescope.builtin').diagnostics(require('plugins.telescope').document)<cr>",
+					"󱪚 DOCUMENT DIAGNOSTICS",
+				},
+				-- D = {"<cmd>lua require('trouble').toggle('document_diagnostics')<cr>",                         "󱪚 TROUBLE DOCUMENT"},
+				w = {
+					"<cmd>lua require('telescope.builtin').diagnostics(require('plugins.telescope'))<cr>",
+					" WORKSPACE DIAGNOSTICS",
+				},
+				-- W = {"<cmd>lua require('trouble').toggle('workspace_diagnostics')<cr>",                         " TROUBLE WORKSPACE"},
+				f = {
+					"<cmd>lua vim.lsp.buf.format({ async = false })<cr>",
+					"󱐀 FORMAT",
+				},
+				i = {
+					"<cmd>LspInfo<cr>",
+					" INFO",
+				},
+				m = {
+					name = " MARKDOWN",
+					p = {
+						"<cmd>Glow<cr>",
+						" MARKDOWN PREVIEW",
+					},
+				},
+				t = {
+					name = "󱁢 TERRAFORM",
+					i = {
+						"<cmd>:!terraform init<cr>",
+						" INIT",
+					},
+					v = {
+						"<cmd>:!terraform validate<cr>",
+						" VALIDATE",
+					},
+					p = {
+						"<cmd>:!terraform plan<cr>",
+						" PLAN",
+					},
+					a = {
+						"<cmd>:!terraform apply -auto-approve<cr>",
+						" APPLY",
+					},
+				},
+			},
+			f = {
+				name = " FIND",
+				f = {
+					"<cmd>lua require('telescope.builtin').find_files(require('plugins.telescope'))<cr>",
+					"󰈞 FILE",
+				},
+				m = {
+					"<cmd>lua require('telescope.builtin').marks(require('plugins.telescope'))<cr>",
+					" MARKS",
+				},
+				M = {
+					"<cmd>lua require('telescope.builtin').man_pages(require('plugins.telescope'))<cr>",
+					" MAN PAGES",
+				},
+				p = {
+					"<cmd>lua require'telescope'.extensions.projects.projects{}<cr>",
+					"󱈆 PROJECTS",
+				},
+				r = {
+					"<cmd>lua require('telescope.builtin').oldfiles(require('plugins.telescope'))<cr>",
+					"󰡦 RECENT FILES",
+				},
+				R = {
+					"<cmd>lua require('telescope.builtin').registers(require('plugins.telescope'))<cr>",
+					"󱝩 REGISTERS",
+				},
+				t = {
+					"<cmd>lua require('telescope.builtin').live_grep(require('plugins.telescope'))<cr>",
+					"󱘢 TEXT",
+				},
+				d = {
+					"<cmd>:TodoTelescope<cr>",
+					" TODO",
+				},
+				H = {
+					"<cmd>:Telescope harpoon marks<cr>",
+					" HARPOON MARKS",
+				},
+				a = {
+					"<cmd>lua require('harpoon.mark').add_file()<cr>",
+					"󱝁 HARPOON ADD",
+				},
+				h = {
+					"<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
+					" HARPOON MENU",
+				},
+			},
+			c = {
+				name = " CONFIG",
+				p = {
+					"<cmd>Lazy profile<cr>",
+					" PROFILE",
+				},
+				s = {
+					"<cmd>Lazy sync<cr>",
+					" SYNC",
+				},
+				m = {
+					"<cmd>Mason<cr>",
+					"󰰑 MASON",
+				},
+				t = {
+					"<cmd>lua require('telescope.builtin').colorscheme(require('plugins.telescope'))<cr>",
+					" THEME",
+				},
+			},
+			g = {
+				name = "󰊢 GIT",
+				-- BRANCHES ONLY WORK INSIDE GIT REP (Maybe catch exception?)
+				b = {
+					"<cmd>:GitBlameToggle<cr>",
+					" BLAME",
+				},
+				c = {
+					"<cmd>lua require('telescope.builtin').git_branches(require('plugins.telescope'))<cr>",
+					" BRANCHES",
+				},
+				l = {
+					"<cmd>lua _lazygit_toggle()<cr>",
+					" LAZYGIT",
+				},
+			},
+		}
+		-- G
+		local g_mappings = {
+			-- STANDARD MAPPINGS
+			["a"] = "Code Action",
+			["d"] = "Go to definition",
+			["t"] = "Go to type definition",
+			["D"] = "Go to declaration",
+			["r"] = "Show references",
+			["l"] = "Start line diagnostics",
+			["i"] = "Go to Implementation",
+			["h"] = "Hover",
+			["f"] = "Go to References | Implementation",
+		}
+		--> APPLY MAPPINGS AND OPTIONS
+		local wk = require("which-key")
+		wk.register(leader_mappings, leader_opts)
+		wk.register(g_mappings, g_opts)
+	end,
 }
-
